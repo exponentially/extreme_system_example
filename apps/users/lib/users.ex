5aec8c88304b2c5898080740da146581c68507c1
Milan Burmaja <burmajam@gmail.com>
2017-05-23 11:38:46 +0200

    $ mix new users --module ExtremeSystem.Example.Users

---
+++ apps/users/lib/users.ex
@@ -0,0 +1,18 @@
+defmodule ExtremeSystem.Example.Users do
+  @moduledoc """
+  Documentation for ExtremeSystem.Example.Users.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExtremeSystem.Example.Users.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/users/test/users_test.exs
@@ -0,0 +1,8 @@
+defmodule ExtremeSystem.Example.UsersTest do
+  use ExUnit.Case
+  doctest ExtremeSystem.Example.Users
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
