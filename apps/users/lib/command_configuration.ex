d171374cf8fac8f0bd2db7db7b1c676658b79d72
Milan Burmaja <burmajam@gmail.com>
2018-09-07 15:15:08 +0200

    more module documentation

--- apps/users/lib/command_configuration.ex
+++ apps/users/lib/command_configuration.ex
@@ -1,6 +1,20 @@
 defmodule ExtremeSystem.Example.Users.CommandConfiguration do
+  @moduledoc """
+  In this module register your aggregate modules with their stream prefix.
+
+  In this case we are registering User aggregate implemented in
+  `ExtremeSystem.Example.Users.Aggregates.User` module. We also specify here prefix
+  `ex_users` for this aggregate.
+
+  That means that when you register new user that gets id: 05c03d0c-b29a-11e8-aff9-acbc32d4fe0b,
+  it's events will be stored in `ex_users-05c03d0c-b29a-11e8-aff9-acbc32d4fe0b` stream of EventStore.
+
+  Because of the stream naming, if projection `by_category` is started in EventStore, there will be
+  indexed stream for all users: `$ce-ex_users`. You can check that one @
+  http://localhost:2113/web/index.html#/streams/$ce-ex_users
+  """
   alias ExtremeSystem.Example.Users.Aggregates
 
   use Extreme.System.CommandConfiguration,
-    aggregates: [{Aggregates.User, "ex_users"}, {Aggregates.Company, "ex_company"}]
+    aggregates: [{Aggregates.User, "ex_users"}]
 end
--- apps/users/lib/example/facade.ex
+++ apps/users/lib/example/facade.ex
@@ -1,5 +1,18 @@
 defmodule ExtremeSystem.Example.Users.Facade do
-  use Extreme.System.Facade, default_cache: 1_000
+  @moduledoc """
+  Facade modules are entry point to your application. Like router in Phoenix project.
+  In this case we specify that for GenServer call {:new, params, metadata} we will be
+  sent to `ExtremeSystem.Example.MessageHandlers.Users.new/2` function.
+
+  Each route call is run in it's own process.
+
+  Here's also specified cache of 1_000ms. Each call is hashed and stored in cache
+  together with it's response. That will stay in cache for 1sec. If you don't won't cache,
+  you can specify `:no_cache` instead of `1_000`.
+  
+  Default caching can be overriden for specific route like here for `:new` route.
+  """
+  use Extreme.System.Facade, default_cache: 1_000, cache_overrides: [new: :no_cache]
 
   alias ExtremeSystem.Example.MessageHandlers, as: MsgHandler
 
